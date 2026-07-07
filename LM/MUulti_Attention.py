6f061d23890d5105d0cf8b0603ce391e3bb01ba8
Isaiah Green <isaiahgreen2022@gmail.com>
2026-07-07 11:05:37 -0500

    added Tokenizer that can handle special/custom charcaters and a multi level attention file for the transformer block later

---
+++ LM/MUulti_Attention.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+class Attention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, num_heads=2, dropout=0.0, qkv_bias=False):
+        super().__init__()
+        assert(d_out % num_heads == 0), "dimentionality Must be divisbale by the number of heads"
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.heads_dim = d_out // num_heads
+        self.weight_q = torch.nn.Linear(d_in , d_out, bias=qkv_bias)
+        self.weight_k = torch.nn.Linear(d_in , d_out, bias=qkv_bias)
+        self.weight_v = torch.nn.Linear(d_in , d_out, bias=qkv_bias)
+        self.out_projection = torch.nn.Linear(d_in , d_out, bias=qkv_bias)
+        self.dropout = torch.nn.Dropout(dropout)
+        # mask goes here
+
+    def forward(self, batch):
+        b, number_tokens, d_z = batch.shape
+
+        queries = self.weight_q(batch)
+        keys =  self.weight_k(batch)
+        values = self.weight_v(batch)
+        ## Adding a dimention equal to number opf heads and use view to reshape it(format a)
+        queries = queries.view(b, number_tokens, self.num_heads, self.heads_dim)
+        keys = keys.view(b, number_tokens, self.num_heads, self.heads_dim)
+        values = values.view(b, number_tokens, self.num_heads, self.heads_dim)
+
+        # transpose to make MM work after the view dimentions were altered
+        #(format b)
+        queries = queries.transpose(1,2)
+        keys = keys.transpose(1,2)
+        values = values.transpose(1,2)
+        ## caluculating attn_scores (keys go format c)
+        attention_score = queries @ keys.transpose(2,3)
+        #mask
+        ## after this annt_wieghts is in format c
+        attention_weights = torch.softmax(attention_score / keys.shape[-1], dim= 1)
+        attention_weights = self.dropout(attention_weights)
+        ##(annt_wieghts put matrix in format d) then transpose puts bac k in format a)
+        context_vector = (attention_weights @ values).transpose(1,2)
+        ##match desired output dimentions using reshape (keep values same jsut change shape)
+        context_vector = context_vector.reshape(b, number_tokens, self.d_out)
+        context_vector - self.out_projection(context_vector)
+
+        return context_vector
+'''
+Testing 
+inputs = torch.tensor([[.5 ,.2 , .3 , .4],
+           [.5 , .6 , .7 , .8],
+           [.4 , .6 , .2 ,.8 ],
+           [ .9 , .7 , .5 , .3],
+           [.10, .11, .12, .13]])
+batch = torch.stack((inputs, inputs), dim=0)
+batch_size, context_legnth, d_in = batch.shape
+d_out = 4
+dropout = 0.2
+context_length = 4
+num_heads = 2
+qkv_bias = False
+mha = Attention(d_in, d_out, context_legnth, num_heads , dropout, qkv_bias)
+c_v = mha(batch)
+print(c_v)
+''' 
+
+
---
+++ LM/Tokenizer_vocab.py
@@ -0,0 +1,67 @@
+import tiktoken
+
+class Tokenizier:
+    def __init__(self):
+        self.tokenizer = tiktoken.get_encoding("gpt2") # tokenizer
+        self.vocab_list = {} # dictionary mapping words to an int number or id
+        self.token_list = {} # same as dictionary but with the words encoded and mapped
+        self.special_list = {} # hold all unique charcaters that arent letters or numbers 
+    def is_english(self,text):
+        try:
+            text.encode(encoding='utf-8').decode('ascii')
+        except UnicodeDecodeError:
+            return False
+        else:
+            return True
+    def special_encoding(self, text):
+        ##using byte pair encoding to add special charcaters(non stadard english) to the vocab_list
+        self.vocab_list.update({text : len(self.vocab_list) + 1})
+        self.token_list.update({text.encode() : len(self.token_list) + 1})
+        return 
+    def encode_and_add(self, text):
+        ### split and store raw text in vocab
+        text = text.split()
+        text = sorted(set(text))
+        ## returns and stores the key values only for comparison
+        keys = self.vocab_list.keys()
+        ## loops and adds and key not already in the diction into both the vocab(uncoded) and the token_list(coded)
+        for i in range(len(text)):
+            if text[i] not in keys:
+                if not self.is_english(text[i]):
+                    self.special_encoding(text[i])
+                else:
+                    self.vocab_list.update({text[i] : len(self.vocab_list) + 1})
+                    ####Encode always returns a list do you must put [0] or the index to get to the actual value and no tthe list itself
+                    self.token_list.update({self.tokenizer.encode(text[i])[0]: len(self.token_list) + 1})  
+        new = list(self.token_list.keys())  
+        return new 
+    def decodes(self, text):
+        final = []
+        for item in text:
+            if isinstance(item, int):
+                t = self.tokenizer.decode([item])
+                final.append(t)
+            if isinstance(item, bytes):
+                t = item.decode("utf-8")
+                final.append(t)
+        return final
+    def encode(self,text):
+        return self.tokenizer.encode(text)
+    def get_vocab_length(self):
+        return len(self.vocab_list)
+    def get_token_list_length(self):
+        return len(self.token_list)
+'''
+Testing 
+import sys
+import io
+sys.stdout  = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+token = Tokenizier()
+sample_text = "Thisw is a 테스트용 텍스트 bu"
+sample_text_2 = " i hate everything here lmao"
+print(token.encode_and_add(sample_text))      
+print(token.decodes(token.encode_and_add(sample_text))) 
+
+'''  
+
+
