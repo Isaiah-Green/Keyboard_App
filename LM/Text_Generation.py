a33df0b60b756fed07f821f28ff6e92a70fca269
Isaiah Green <isaiahgreen2022@gmail.com>
2026-07-17 14:12:43 -0500

    Added the text generation file, to tokenizer fomr text and get text form tokens as well as print after sending thro

---
+++ LM/Text_Generation.py
@@ -0,0 +1,66 @@
+import torch
+from Tokenizer_vocab import Tokenizier
+from Model import Model
+
+
+def To_token(tokenizer, input):
+    if input[:3] == "CFUI":
+        token = tokenizer.special_encode(input)
+    else:
+        token = tokenizer.encode(input)
+    token = torch.tensor(token).unsqueeze(0)
+    return token
+        
+def To_ouput(tokenizer , tokens):
+    decode = tokens.squeeze(0)
+    return tokenizer.decodes(decode.tolist())
+
+def Generate_output_highest(model,idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]# gets last row
+        probability = torch.softmax(logits, dim=-1)
+        # use argmax to get highest index value
+        idx_next = torch.argmax(probability, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=-1)
+    return idx
+
+def Generate_output_and_print(model, tokenizer, start_context, device):
+    model.eval()
+    context_size = model.pos_embedding.weight.shape[0]
+    if "CFUI" in start_context:
+        encoded = torch.tensor(tokenizer.special_encode(start_context)).unsqueeze(0)
+    else:
+        encoded = torch.tensor(tokenizer.encode(start_context)).unsqueeze(0)
+        print(encoded)
+    with torch.no_grad():
+        token_ids = Generate_output_highest(model,idx=encoded,max_new_tokens=1, context_size=context_size)
+    decode = To_ouput(tokenizer, token_ids)
+    print(decode)
+    return decode 
+'''
+Testing
+
+cfg = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "emb_dim": 768,
+    "num_heads": 12,
+    "n_layers": 12,
+    "dropout": 0.1,
+    "qkv_bias": False
+}
+model = Model(cfg)
+tokenizer = Tokenizier()
+start_context = "The Wind is Nice"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+to_token = To_token(tokenizer, start_context)
+to_output = To_ouput(tokenizer,to_token)
+print(to_token)
+print(to_output)
+final = Generate_output_and_print(model, tokenizer, start_context, device)
+print(final)
+'''
